d6db90fb88f12a6f58c666348741701555cc9638
mastre <peaceyop@gmail.com>
2023-07-25 09:35:24 +0900

    [Silver V] Title: 영화감독 숌, Time: 120 ms, Memory: 12384 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val n = readln().toInt()
+    var searchCount = 0
+    var currentNumber = 665
+
+    while (searchCount < n) {
+        currentNumber++
+        var temp = currentNumber
+        
+        while(temp > 665){
+            if(temp % 1000 == 666) {
+                searchCount++
+                break
+            } else {
+                temp /= 10
+            }
+        }
+    }
+    println(currentNumber)
+}
