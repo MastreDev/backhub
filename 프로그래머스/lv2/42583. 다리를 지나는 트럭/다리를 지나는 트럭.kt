ebc425d634bf0855f5ce289ebd57611ccd8fc9e9
mastre <peaceyop@gmail.com>
2023-09-11 20:45:40 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 19.06 ms, Memory: 63.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.kt
@@ -3,23 +3,17 @@ import java.util.*
 class Solution {
     fun solution(bridge_length: Int, weight: Int, truck_weights: IntArray): Int {
         val truckQueue : Queue<Int> = truck_weights.toCollection(LinkedList())
-        val onBridge : Queue<Int> = LinkedList()
+        val onBridge : Queue<Int> = IntArray(bridge_length).toCollection(LinkedList())
         var time = 0
         
         while(truckQueue.isNotEmpty()) {
+            time++
+            onBridge.remove()
             val current = truckQueue.peek()!!
             if(onBridge.sum() + current <= weight) {
                 onBridge.offer(truckQueue.poll()!!)
-                time++
-            } else if(onBridge.size > bridge_length) {
-                onBridge.remove()
-                
-            } else if (onBridge.size == bridge_length && onBridge.peek() > 0) {
-                onBridge.remove()
-                
             } else {
                 onBridge.offer(0)
-                time++
             }
         }
         
