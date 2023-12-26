b6c516bb2534a9390f078887385588e9d98e4467
mastre <peaceyop@gmail.com>
2023-12-26 10:16:56 +0900

    [Bronze V] Title: 시험 성적, Time: 88 ms, Memory: 12368 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.kt
@@ -0,0 +1,9 @@
+fun main() {
+    when (readln().toInt()) {
+        in 90..100 -> 'A'
+        in 80..89 -> 'B'
+        in 70..79 -> 'C'
+        in 60..69 -> 'D'
+        else -> 'F'
+    }.let { println(it) }
+}
