97a5aafa7435dbd0d110c2a13aeace04a66593f5
mastre <peaceyop@gmail.com>
2023-12-28 11:59:46 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 100 ms, Memory: 13004 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.kt
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.kt
@@ -1,29 +1,22 @@
 fun main() {
+    fun StringBuilder.space(count: Int): StringBuilder = this.apply { repeat(count * 4) { this.append("_") } }
     val n = readln().toInt()
     val result = StringBuilder()
     result.appendLine("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.")
-
     fun recursive(count: Int) {
         if (count == n) {
-            result.addSpace(count).appendLine("\"재귀함수가 뭔가요?\"")
-            result.addSpace(count).appendLine("\"재귀함수는 자기 자신을 호출하는 함수라네\"")
-            result.addSpace(count).appendLine("라고 답변하였지.")
+            result.space(count).appendLine("\"재귀함수가 뭔가요?\"")
+            result.space(count).appendLine("\"재귀함수는 자기 자신을 호출하는 함수라네\"")
+            result.space(count).appendLine("라고 답변하였지.")
             return
         }
-        result.addSpace(count).appendLine("\"재귀함수가 뭔가요?\"")
-        result.addSpace(count).appendLine("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.")
-        result.addSpace(count).appendLine("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.")
-        result.addSpace(count).appendLine("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"")
+        result.space(count).appendLine("\"재귀함수가 뭔가요?\"")
+        result.space(count).appendLine("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.")
+        result.space(count).appendLine("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.")
+        result.space(count).appendLine("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"")
         recursive(count + 1)
-        result.addSpace(count).appendLine("라고 답변하였지.")
+        result.space(count).appendLine("라고 답변하였지.")
     }
-
     recursive(0)
     print(result)
-
-}
-
-fun StringBuilder.addSpace(count: Int): StringBuilder {
-    repeat(count * 4) { this.append("_") }
-    return this
 }
