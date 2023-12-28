2e8726f74a91da430f96dd6bf80908a135dcf23a
mastre <peaceyop@gmail.com>
2023-12-28 23:03:13 +0900

    [Silver II] Title: 마인크래프트, Time: 888 ms, Memory: 55864 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.kt
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.kt
@@ -7,14 +7,13 @@ fun main() {
     }
     val allBlock = b + map.sumOf { it.sum() }
     val maxTargetHeight = allBlock / (n * m)
-//    val results = mutableListOf<Pair<Int, Int>>()
     var minSeconds = Int.MAX_VALUE
     var floor = 0
 
-    val searchRange = maxTargetHeight downTo 0
+    val searchRange = 0 .. maxTargetHeight
     searchRange.forEach {
         var time = 0
-        for (i in 0 until n) {
+        stop@ for (i in 0 until n) {
             for (j in 0 until m) {
                 val gapBlock = it - map[i][j]
                 if (gapBlock > 0) {
@@ -22,22 +21,13 @@ fun main() {
                 } else if (gapBlock < 0) {
                     time += (gapBlock.absoluteValue * 2)
                 }
+                if(time > minSeconds) break@stop  
             }
         }
-        if(minSeconds == time){
-            floor = maxOf(it, floor)
-        }
-
-        if(minSeconds > time){
+        if(minSeconds >= time){
             minSeconds = time
             floor = it
         }
-
-//        results.add(time to it)
     }
     println("$minSeconds $floor")
-//    val minTime = results.minOf { it.first }
-//    results.filter { it.first == minTime }
-//        .maxByOrNull { it.second }
-//        ?.also { println("${it.first} ${it.second}") }
 }
