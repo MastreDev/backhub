88a8c9c6b8922d693e41964beb6ac5259839c5d1
mastre <peaceyop@gmail.com>
2024-01-09 11:01:11 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 960 ms, Memory: 62640 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.kt
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.kt
@@ -1,59 +1,39 @@
-import java.util.LinkedList
-
 fun main() {
     val (n, m, r) = readln().split(" ").map(String::toInt)
     val board = Array(n) { readln().split(" ").map(String::toInt).toIntArray() }
-    val queues = mutableListOf<LinkedList<Int>>()
 
-    fun makeQueue(skip: Int) {
-        if (queues.flatMap { it.toList() }.size == n * m) return
-        val queue = LinkedList<Int>()
+    fun rotate(skip: Int) {
+        val remain = board[skip][skip]
+        val rowEnd = n - skip
+        val colEnd = m - skip
 
-        for (i in skip until m - skip) {
-            queue.add(board[skip][i])
+        for (i in skip + 1 until m - skip) {
+            board[skip][i - 1] = board[skip][i]
         }
         for (i in skip + 1 until n - skip) {
-            queue.add(board[i][m - 1 - skip])
+            board[i - 1][colEnd - 1] = board[i][colEnd - 1]
         }
-        for (i in m - skip - 2 downTo skip) {
-            queue.add(board[n - 1 - skip][i])
+        for (i in colEnd - 1 downTo skip + 1) {
+            board[rowEnd - 1][i] = board[rowEnd - 1][i - 1]
         }
-        for (i in n - skip - 2 downTo skip + 1) {
-            queue.add(board[i][skip])
+        for (i in rowEnd - 1 downTo skip + 1) {
+            board[i][skip] = board[i - 1][skip]
         }
-        queues.add(queue)
-        makeQueue(skip + 1)
+        board[skip + 1][skip] = remain
     }
 
-    fun reComposition() {
-        queues.forEachIndexed { index, ints ->
-            for (i in index until m - index) {
-                board[index][i] = ints.poll()!!
-            }
-            for (i in index + 1 until n - index) {
-                board[i][m - 1 - index] = ints.poll()!!
-
-            }
-            for (i in m - index - 2 downTo index) {
-                board[n - 1 - index][i] = ints.poll()!!
-            }
-            for (i in n - index - 2 downTo index + 1) {
-                board[i][index] = ints.poll()!!
-            }
+    val innerCircleCount = minOf(n, m) / 2
+    repeat(r) { _ ->
+        repeat(innerCircleCount) {
+            rotate(it)
         }
     }
 
-    fun rotate() {
-        queues.forEach {
-            it.addLast(it.removeFirst())
+    board
+        .fold(StringBuilder()) { acc, row ->
+            acc.appendLine(row.joinToString(" "))
         }
-    }
-
-    makeQueue(0)
-
-    repeat(r) { rotate() }
-
-    reComposition()
-
-    board.forEach { println(it.joinToString(" ")) }
+        .also {
+            println(it.toString())
+        }
 }
