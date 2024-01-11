bfca5acd865dd6a9c251a5d5debf7dc53308d0e9
mastre <peaceyop@gmail.com>
2024-01-11 15:36:58 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 2176 ms, Memory: 807456 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.kt
@@ -0,0 +1,48 @@
+fun main() {
+
+    val (n, k) = readln().split(" ").map(String::toInt)
+    val array = readln().split(" ").map(String::toInt).toIntArray()
+    val temp = IntArray(array.size)
+    
+    var saveCount = 0
+    val history = hashMapOf<Int, Int>()
+
+    fun Int.record(): Int {
+        history[++saveCount] = this
+        return this
+    }
+
+    fun merge(array: IntArray, p: Int, q: Int, r: Int) {
+        var i = p
+        var j = q + 1
+        var t = -1
+
+        while (i <= q && j <= r) {
+            temp[++t] = array[if (array[i] > array[j]) j++ else i++].record()
+        }
+        while (i <= q) {
+            temp[++t] = array[i++].record()
+        }
+        while (j <= r) {
+            temp[++t] = array[j++].record()
+        }
+        i = p
+        t = -1
+        while (i <= r) {
+            array[i++] = temp[++t]
+        }
+    }
+
+    fun mergeSort(array: IntArray, p: Int = 0, r: Int = array.lastIndex) {
+        if (p >= r) return
+        val q = (p + r) / 2
+        mergeSort(array, p, q)
+        mergeSort(array, q + 1, r)
+        merge(array, p, q, r)
+        if(history.size >= k) return
+    }
+
+    mergeSort(array)
+    println(history.getOrDefault(k, -1))
+
+}
