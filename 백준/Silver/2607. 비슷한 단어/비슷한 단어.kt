2abdc24c932008a7e94ff5da392c1bd26ec35a85
mastre <peaceyop@gmail.com>
2024-01-25 15:52:11 +0900

    [Silver II] Title: 비슷한 단어, Time: 180 ms, Memory: 26472 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.kt
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.kt
@@ -1,29 +1,22 @@
 fun main() {
-    // 하나의 문자를 더하거나, 빼거나, 바꾼다
     val n = readln().toInt()
-    val words = Array(n) { readln().toList() }
-    // 하나의 아무 문자를 더해서 구성이 같아지거나
-    // 하나의 아무 문자를 빼서 구성이 같아지거나
-    // 하나의 아무 문자를 바꿔서 구성이 같아져야 한다
-
-    val word = words[0].toList()
+    val words = Array(n) { readln() }
+    val word = words[0]
     var similarCount = 0
 
     words.drop(1).forEach { other ->
         val tempWord = word.toMutableList().also { it.sort() }
         val tempOther = other.toMutableList().also { it.sort() }
-        if (tempWord.size <= tempOther.size) {
-            if (tempWord.isSimilar(tempOther)) similarCount++
-        } else {
-            if (tempOther.isSimilar(tempWord)) similarCount++
-        }
+
+        val (short, long) = if (tempWord.size <= tempOther.size) tempWord to tempOther else tempOther to tempWord
+        if (short.isSimilar(long)) similarCount++
     }
     println(similarCount)
 }
 
-private fun MutableList<Char>.isSimilar(other: MutableList<Char>): Boolean {
+private fun MutableList<Char>.isSimilar(bigger: MutableList<Char>): Boolean {
     while (this.isNotEmpty()) {
-        other.remove(this.removeFirst())
+        bigger.remove(this.removeFirst())
     }
-    return other.isEmpty() || other.size == 1
+    return bigger.size < 2
 }
