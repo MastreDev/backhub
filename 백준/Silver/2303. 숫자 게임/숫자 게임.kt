cb0e33923c28db8fa5051d81e46b975391979e80
mastre <peaceyop@gmail.com>
2024-01-27 10:56:09 +0900

    [Silver V] Title: 숫자 게임, Time: 184 ms, Memory: 21480 KB -BaekjoonHub

--- 백준/Silver/2303. 숫자 게임/숫자 게임.kt
+++ 백준/Silver/2303. 숫자 게임/숫자 게임.kt
@@ -1,29 +1,24 @@
 fun main() {
     val n = readln().toInt()
-
-    var maxNumber = 0
-    fun doRecursive(depth: Int, index: Int, numbers: IntArray, con: IntArray) {
-        if (depth == 3) {
-            maxNumber = maxOf(maxNumber, con.sum() % 10)
-            return
-        }
-
-        for (i in index until numbers.size) {
-            con[depth] = numbers[i]
-            doRecursive(depth + 1, i + 1, numbers, con)
-        }
-    }
-
     var winNumber = 0
     var winSum = 0
-    repeat(n) {
-        maxNumber = 0
-        val p = readln().split(" ").map(String::toInt).toIntArray()
-        doRecursive(0, 0, p, IntArray(3))
-        winSum = maxOf(winSum, maxNumber)
-        if (winSum == maxNumber) winNumber = it + 1
+    for (i in 0 until n) {
+        val maxSum = readln().split(" ").map(String::toInt).toIntArray().getSum()
+        if (winSum > maxSum) continue
+        winSum = maxSum
+        winNumber = i + 1
     }
-
     println(winNumber)
+}
 
+private fun IntArray.getSum(): Int {
+    var max = 0
+    for (i in 0 until 3) {
+        for (j in i + 1 until 4) {
+            for (k in j + 1 until 5) {
+                max = maxOf(max, (this[i] + this[j] + this[k]) % 10)
+            }
+        }
+    }
+    return max
 }
