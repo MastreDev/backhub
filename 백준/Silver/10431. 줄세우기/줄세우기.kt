7da9f110cbb0fa6eabc71bdf6d2b24391d246637
mastre <peaceyop@gmail.com>
2024-01-30 22:53:47 +0900

    [Silver V] Title: 줄세우기, Time: 312 ms, Memory: 32640 KB -BaekjoonHub

--- 백준/Silver/10431. 줄세우기/줄세우기.kt
+++ 백준/Silver/10431. 줄세우기/줄세우기.kt
@@ -6,18 +6,17 @@ private val bw = System.out.bufferedWriter()
 fun main() {
     // 키 순서 1번 부터 시작. 항상 20명 같은 키 없음
     repeat(readln().toInt()) {
-        val students = br.readLine().split(" ").map(String::toInt)
+        val students = br.readLine().split(" ")
         val testNumber = students.first()
         val queue = LinkedList<Int>()
         val temp = LinkedList<Int>()
         var walkCount = 0
 
         for (i in 1 until students.size) {
-            val student = students[i]
+            val student = students[i].toInt()
 
             while (queue.isNotEmpty()) {
-                val last = queue.peek()!!
-                if (last > student) break
+                if (queue.peek()!! > student) break
                 temp.offer(queue.poll()!!)
             }
 
