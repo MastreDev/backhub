bcbbb382d0ad29f81cea90bcce04ae81c6928e58
mastre <peaceyop@gmail.com>
2024-01-30 17:20:08 +0900

    [Silver III] Title: NBA 농구, Time: 152 ms, Memory: 15660 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.kt
+++ 백준/Silver/2852. NBA 농구/NBA 농구.kt
@@ -2,72 +2,53 @@ import java.text.DecimalFormat
 
 fun main() {
     val n = readln().toInt()
-    val record = Array(n) { readln().split(" ") }
-    val duration = "48:00"
-    var aTeamWinDuration = "00:00"
-    var bTeamWinDuration = "00:00"
-    var score = arrayOf(0, 0)
-    var lastGoalTime = "00:00"
+    val record = Array(n) {
+        val (team, rTime) = readln().split(" ")
+        val (rHour, rMin) = rTime.split(":")
+        val time = rHour.toInt() * 60 + rMin.toInt()
+        arrayOf(team.toInt(), time)
+    }
+    val duration = 48 * 60
+    var aTeamWinDuration = 0
+    var bTeamWinDuration = 0
+    val score = arrayOf(0, 0)
+    var lastGoalTime = 0
 
     record.forEach {
-        val team = it[0].toInt()
+        val team = it[0]
         val time = it[1]
 
-        if (score[0] == score[1]) {
-            // 이전까지 동점,
-            score[team - 1]++ // 득점 기록
-            lastGoalTime = time
+        when {
+            score[0] < score[1] -> {
+                score[team - 1]++ // 득점 기록
+                bTeamWinDuration += time - lastGoalTime
+                lastGoalTime = time
+            }
 
-        } else if (score[0] > score[1]) {
-            // a팀이 이기고 있는 상황
-            score[team - 1]++ // 득점 기록
-            val gap = time.minusTime(lastGoalTime)
-            aTeamWinDuration = aTeamWinDuration.plusTime(gap)
-            lastGoalTime = time
+            score[0] > score[1] -> {
+                score[team - 1]++ // 득점 기록
+                aTeamWinDuration += time - lastGoalTime
+                lastGoalTime = time
+            }
 
-        } else {
-            // b팀이 이기고 있는 상황
-            score[team - 1]++ // 득점 기록
-            val gap = time.minusTime(lastGoalTime)
-            bTeamWinDuration = bTeamWinDuration.plusTime(gap)
-            lastGoalTime = time
+            else -> {
+                score[team - 1]++ // 득점 기록
+                lastGoalTime = time
+            }
         }
     }
 
     if (score[0] > score[1]) {
-        val gap = duration.minusTime(lastGoalTime)
-        aTeamWinDuration = aTeamWinDuration.plusTime(gap)
+        aTeamWinDuration += duration - lastGoalTime
 
     } else if (score[0] < score[1]) {
-        val gap = duration.minusTime(lastGoalTime)
-        bTeamWinDuration = bTeamWinDuration.plusTime(gap)
+        bTeamWinDuration += duration - lastGoalTime
     }
 
-    println(aTeamWinDuration)
-    println(bTeamWinDuration)
-
+    println("${aTeamWinDuration.toResult()}\n${bTeamWinDuration.toResult()}")
 }
 
-private fun String.minusTime(before: String): String {
-    val thisHour = this.split(":")[0].toInt()
-    val thisMin = this.split(":")[1].toInt()
-
-    val otherHour = before.split(":")[0].toInt()
-    val otherMin = before.split(":")[1].toInt()
-
-    val gap = thisHour * 60 + thisMin - otherHour * 60 - otherMin
-    val format = DecimalFormat("00")
-    return "${format.format(gap / 60)}:${format.format(gap % 60)}"
-}
-
-private fun String.plusTime(before: String): String {
-    val thisHour = this.split(":")[0].toInt()
-    val thisMin = this.split(":")[1].toInt()
-
-    val otherHour = before.split(":")[0].toInt()
-    val otherMin = before.split(":")[1].toInt()
-
-    val gap = thisHour * 60 + thisMin + otherHour * 60 + otherMin
-    val format = DecimalFormat("00")
-    return "${format.format(gap / 60)}:${format.format(gap % 60)}"
+private val format = DecimalFormat("00")
+private fun Int.toResult(): String {
+    return "${format.format(this / 60)}:${format.format(this % 60)}"
 }
