b6d35787d28717cf0d5b2f5003084c82cf439202
mastre <peaceyop@gmail.com>
2024-01-30 11:50:51 +0900

    [Silver IV] Title: 학생 번호, Time: 208 ms, Memory: 19512 KB -BaekjoonHub

--- 백준/Silver/1235. 학생 번호/학생 번호.kt
+++ 백준/Silver/1235. 학생 번호/학생 번호.kt
@@ -1,13 +1,14 @@
 fun main() {
     val n = readln().toInt()
-    val students = Array(n) { readln().toList() }
-    val length = students.first().size
+    val students = List(n) { readln() }
+    val length = students[0].length
+    val sets = mutableSetOf<String>()
     var result = 0
 
     outer@ for (i in 1..length) {
-        val sets = mutableSetOf<String>()
+        sets.clear()
         for (j in students.indices) {
-            val isDup = !sets.add(students[j].takeLast(i).joinToString(""))
+            val isDup = !sets.add(students[j].takeLast(i))
             if (isDup) continue@outer
         }
         result = i
