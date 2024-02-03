c6cf156f1605c16231277fcec8eba3abf3faf617
mastre <peaceyop@gmail.com>
2024-02-03 18:49:29 +0900

    [Silver II] Title: 싸이버개강총회, Time: 592 ms, Memory: 82836 KB -BaekjoonHub

--- 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.kt
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.kt
@@ -11,19 +11,19 @@ fun main() {
     val joiners = mutableSetOf<String>()
     var result = 0
 
-    val start = Time(s)
-    val end = Time(e)
-    val streamingEnd = Time(q)
+    val start = s.toSeconds()
+    val end = e.toSeconds()
+    val streamingEnd = q.toSeconds()
 
     while (true) {
         val input = br.readLine()
         if (input == null || input.isEmpty()) break
         val split = input.split(" ")
-        val time = Time(split[0])
+        val time = split[0].toSeconds()
         val id = split[1]
         if (time <= start) {
             joiners.add(id)
-        } else if (end <= time && time <= streamingEnd) {
+        } else if (time in end..streamingEnd) {
             if (joiners.remove(id)) (result++)
         }
     }
@@ -32,16 +32,6 @@ fun main() {
     br.close()
 }
 
-private data class Time(val hour: Int, val min: Int) {
-
-    constructor(raw: String) : this(raw.split(":")[0].toInt(), raw.split(":")[1].toInt())
-
-    operator fun compareTo(other: Time): Int {
-        if (hour < other.hour) return -1
-        if (hour > other.hour) return 1
-        if (min < other.min) return -1
-        if (min > other.min) return 1
-        return 0
-    }
-
+private fun String.toSeconds(): Int {
+    return this.split(":").map(String::toInt).run { this[0] * 60 + this[1] }
 }
